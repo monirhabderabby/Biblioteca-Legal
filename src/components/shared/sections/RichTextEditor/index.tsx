453490217b504e8ff1823bba14ac9586599bae24
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-23 18:16:37 +0600

    added: rich text edit

---
+++ src/components/shared/sections/RichTextEditor/index.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import BulletList from "@tiptap/extension-bullet-list";
+import Heading from "@tiptap/extension-heading";
+import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
+import OrderedList from "@tiptap/extension-ordered-list";
+import TextAlign from "@tiptap/extension-text-align";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import ImageResize from "tiptap-extension-resize-image";
+import ToolBar from "./ToolBar";
+
+interface RichTextEditorProps {
+  content: string;
+  onChange: (content: string) => void;
+}
+
+export default function RichTextEditor({
+  content,
+  onChange,
+}: RichTextEditorProps) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure(),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Heading.configure({
+        levels: [1, 2, 3],
+      }),
+      OrderedList.configure({
+        HTMLAttributes: {
+          class: "list-decimal ml-3",
+        },
+      }),
+      BulletList.configure({
+        HTMLAttributes: {
+          class: "list-disc ml-3",
+        },
+      }),
+      Highlight,
+      Image,
+      ImageResize,
+    ],
+    content,
+    editorProps: {
+      attributes: {
+        class: "min-h-[156px] border rounded-md bg-slate-50 py-2 px-3",
+      },
+    },
+    onUpdate({ editor }) {
+      onChange(editor.getHTML());
+    },
+  });
+
+  return (
+    <div>
+      <ToolBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+}
