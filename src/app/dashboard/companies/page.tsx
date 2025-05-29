256034279e417b6797180a2b203f5e8d9b1c81e4
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-29 14:25:22 +0600

    developed: create company feature

---
+++ src/app/dashboard/companies/page.tsx
@@ -0,0 +1,20 @@
+import AddCompanyModal from "@/components/shared/modals/add-company-modal";
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <div className="w-full flex justify-between mb-5">
+        <h1 className="text-primary font-semibold text-[32px] leading-[120%]">
+          Manage Company
+        </h1>
+
+        <AddCompanyModal
+          trigger={<Button className="h-[45px]">Add Company</Button>}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
