4a26dc602ded1eccc76179efb38318691fd19136
unknown <monirhrabby.personal@gmail.com>
2025-08-22 22:25:30 +0600

    added: upstach

--- src/app/(website)/collections/[id]/page.tsx
+++ src/app/(website)/collections/[id]/page.tsx
@@ -1,28 +1,38 @@
 import { auth } from "@/auth";
 import { getCurrentUserSubscription } from "@/helper/subscription";
 import { prisma } from "@/lib/db";
+import { cache } from "@/lib/redis/cache";
 import { notFound } from "next/navigation";
 import ArticleContainer from "./_components/article-container";
 import CollectionHeader from "./_components/collection-header";
 
-export const revalidate = 3600;
+export const revalidate = 86400;
 
+// Cache static params to reduce DB queries
 export async function generateStaticParams() {
-  const ids = await prisma.document.findMany({
-    select: {
-      id: true,
+  const ids = await cache(
+    "document_ids",
+    async () => {
+      const res = await prisma.document.findMany({
+        select: { id: true },
+      });
+      return res;
     },
-  });
-
+    86400
+  ); // cache 1 day
   return ids;
 }
 
 const Page = async ({ params }: { params: { id: string } }) => {
-  const document = await prisma.document.findUnique({
-    where: {
-      id: params.id,
+  const document = await cache(
+    `document:${params.id}`,
+    async () => {
+      return await prisma.document.findUnique({
+        where: { id: params.id },
+      });
     },
-  });
+    86400
+  ); // 1 hour cache
 
   const cu = await auth();
 
