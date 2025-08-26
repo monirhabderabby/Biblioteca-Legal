4f035b24941bc79e121e02dafdaa873725114d19
unknown <monirhrabby.personal@gmail.com>
2025-08-26 06:33:06 +0600

    a

--- src/lib/paddle.ts
+++ src/lib/paddle.ts
@@ -1,7 +1,6 @@
-import { Environment, LogLevel, Paddle } from "@paddle/paddle-node-sdk";
+import { Environment, Paddle } from "@paddle/paddle-node-sdk";
 const apiKey = process.env.PADDLE_API_KEY!;
 
 export const paddle = new Paddle(apiKey, {
   environment: Environment.production, // or Environment.sandbox for accessing sandbox API
-  logLevel: LogLevel.verbose, // or LogLevel.error for less verbose logging
 });
